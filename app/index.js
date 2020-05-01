00f56129203bf82c0a05e1379d0df3a3204516aa
Paul Smith <Psmith4422@conestogac.on.ca>
2020-05-01 19:22:01 -0400

    Added navbar and some styling

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Popular from './components/Popular'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello World!
+      <div className='container'>
+        <Popular/>
       </div>
     )
   }
@@ -15,4 +16,4 @@ class App extends React.Component {
 ReactDOM.render(
   <App />,
   document.getElementById('app')
-) 
+)
